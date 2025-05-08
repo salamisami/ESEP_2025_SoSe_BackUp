12c001b696193bddc9e1df46df5174bf121793ee
Maverick <maverick.widjaja@haw-hamburg.de>
2025-05-08 18:36:04 +0200

    added reaction to stop button

--- main.cpp
+++ main.cpp
@@ -6,22 +6,22 @@
 using namespace std;
 
 
-void wait(int seconds){
+void wait(int seconds) {
 	usleep(ONE_MILLISECOND * 1000 * seconds);
 }
 
 int main() {
 	cout << "Starting Program..." << endl; // prints Hello World!!!
 	int globalChannelID = ChannelCreate(0);
-	int globalConID = ConnectAttach(0,0,globalChannelID,_NTO_SIDE_CHANNEL, 0);
+	int globalConID = ConnectAttach(0, 0, globalChannelID, _NTO_SIDE_CHANNEL, 0);
 
 	HAL* hal = new HAL(globalConID);
-	std::cout << "Festo is gate: " <<std::boolalpha << hal->isGate() << std::endl;
+	std::cout << "Festo is gate: " << std::boolalpha << hal->isGate() << std::endl;
 	//schläger:50600801
 	//weiche   50600801
 	//hal->test_outs();
 	hal->test_ins(globalChannelID);
-	
+
 	delete hal;
 
 	cout << "Program Finished." << endl;
