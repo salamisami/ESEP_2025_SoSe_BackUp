904240ce8f2eef7ca50e23cb5c3cf05291faf1b1
Sami Askar <sami.askar@haw-hamburg.de>
2025-05-25 20:57:03 +0200

    Adjusted traffic to use thread_com

--- inc/TrafficUtility.h
+++ inc/TrafficUtility.h
@@ -7,15 +7,19 @@
 #include <condition_variable>
 #include <functional>
 
+#include "Thread_COM.h"
+#include "Event.h"
+
 class TrafficUtility {
 public:
     // Get the singleton instance
-    static TrafficUtility& getInstance();
+    static TrafficUtility& getInstance(int chid = 0);
 
+    ~TrafficUtility();
     // Traffic light control functions
-    void trafficGreen(int frequency);
-    void trafficYellow(int frequency);
-    void trafficRed(int frequency);
+    void trafficGreen(double frequency);
+    void trafficYellow(double frequency);
+    void trafficRed(double frequency);
 
     // Stop all traffic lights
     void stopAll();
@@ -31,15 +35,21 @@ public:
     // Prevent copying
     TrafficUtility(const TrafficUtility&) = delete;
     TrafficUtility& operator=(const TrafficUtility&) = delete;
+    TrafficUtility(TrafficUtility&&) = delete;
+    TrafficUtility& operator=(TrafficUtility&&) = delete;
 
 private:
-    TrafficUtility();
-    ~TrafficUtility();
+    TrafficUtility(int chid);
+    int coid_;
+    static std::mutex instanceMutex_;
+    static std::unique_ptr<TrafficUtility> instance_;
 
     // Worker functions
     void greenWorker();
     void yellowWorker();
     void redWorker();
+
+    void sendLightPulse(ActuatorEnum state);
 
     // Thread control structure
     struct TrafficLight {
@@ -48,7 +58,7 @@ private:
         std::condition_variable cv;
         std::atomic<bool> stopFlag{false};
         std::atomic<bool> running{false};
-        std::atomic<int> frequency{0};
+        std::atomic<double> frequency{0};
     };
 
     TrafficLight green_;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "TrafficUtility.h"
+#include "Thread_COM.h"
 
 #define ONE_MILLISECOND 1000
 
@@ -11,12 +12,12 @@ using namespace std;
 int main() {
     auto& traffic = TrafficUtility::getInstance();
     
-    traffic.trafficGreen(1);  // Green light at 1Hz
+    traffic.trafficGreen(0.5);  // Green light at 1Hz
     traffic.trafficYellow(2); // Yellow light at 2Hz
-    traffic.trafficRed(0.5);  // Red light at 0.5Hz
+    traffic.trafficRed(5);  // Red light at 0.5Hz
     
     // Let them run for 10 seconds
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(100));
     
     traffic.stopAll();
     return 0;
