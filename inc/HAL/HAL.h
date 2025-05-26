ecc5ea57a4d709e3ed0b698cc3f93776ce2dec0f
Maverick <maverick.widjaja@haw-hamburg.de>
2025-05-26 13:53:20 +0200

    added DoxyGen to HAL

--- inc/HAL/HAL.h
+++ inc/HAL/HAL.h
@@ -14,12 +14,24 @@
 
 class HAL {
 public: //============================================ contructors & destructors ============================================
+	/**
+	 * @brief creates a hal without connection. Use this for test purposes
+	 */
 	HAL();
+
+	/**
+	 * @brief creates a hal with connection. The connection can be mocked, qnet, or gns. (Because they are all using the same interface)
+	 * @param local_receiver pointer to receiver, used by HAL to receive events
+	 * @param local_sender pointer so sender, used by HAL to send events
+	 */
 	HAL(I_Receiver* local_receiver, I_Sender* local_sender);
 	virtual ~HAL();
 
 
 public: //================================================ public functions ================================================
+	/**
+	 * @brief Tests the Interrupt and Actuators. Use this without external connection is recommended
+	 */
 	void test_ins();
 
 private: //================================================ private variables ================================================
