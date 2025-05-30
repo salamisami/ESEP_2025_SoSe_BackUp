28c424446e419f642d754c72ffdc58a40afa7f21
Maverick <maverick.widjaja@haw-hamburg.de>
2025-05-30 14:58:53 +0200

    adjusted Logic class constructor

--- inc/FSM/Logic.h
+++ inc/FSM/Logic.h
@@ -8,18 +8,13 @@
 #include "ModeHandler.h"
 #include "Event.h"
 
-#ifdef MOCK
 #include "Mock_PM.h"
-#else
-#include "PulseMsg.h"
-#endif
 
 #include <thread>
 
 class Logic {
 public: //============================================ contructors & destructors ============================================
-	Logic(Mock_PM::Receiver* local_receiver, Mock_PM::Sender* local_sender, I_Sender* timer_sender);
-    Logic(I_Receiver* local_receiver, I_Sender* local_sender);
+    Logic(I_Receiver* local_receiver, I_Sender* local_sender, I_Sender* timer_sender);
 	//Logic(I_Receiver* local_receiver, I_Sender* local_sender, I_Sender* timer_sender);
     virtual ~Logic();
 	
