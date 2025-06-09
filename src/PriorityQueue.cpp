8da08288f3b2d95ae7e4ae9f303224d7a968c454
Sami Askar <sami.askar@haw-hamburg.de>
2025-06-09 20:25:29 +0200

    added base class for com & prioQueue

---
+++ src/PriorityQueue.cpp
@@ -0,0 +1,31 @@
+#include "PriorityQueue.h"
+
+// Constructor
+PriorityQueue::PriorityQueue() {}
+
+// Add an element to the priority queue
+void PriorityQueue::enqueue(int value) {
+    pq.push(value);
+}
+
+// Remove and return the highest priority element
+int PriorityQueue::dequeue() {
+    if (!pq.empty()) {
+        int value = pq.top();
+        pq.pop();
+        return value;
+    } else {
+        std::cerr << "Priority queue is empty!" << std::endl;
+        return -1; // Indicate that the queue is empty
+    }
+}
+
+// Check if the priority queue is empty
+bool PriorityQueue::isEmpty() const {
+    return pq.empty();
+}
+
+// Get the size of the priority queue
+size_t PriorityQueue::size() const {
+    return pq.size();
+}
