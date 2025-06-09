c63da08455da7a6807d9aae139d83a09b55329e4
Sami Askar <sami.askar@haw-hamburg.de>
2025-06-09 20:25:29 +0200

    added base class for com & prioQueue

---
+++ inc/QNet/COM.h
@@ -0,0 +1,15 @@
+#ifndef COM_H
+#define COM_H
+#pragma once
+
+#include "EventPriority.h"
+#include <cstdint>
+
+public:
+private:
+	I_Receiver* _server;
+	I_Sender* _client;
+    I_Receiver* _dispatcherRec;
+	I_Sender* _dispatcherSen;
+    _FIFO
+#endif
