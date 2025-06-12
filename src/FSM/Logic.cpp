0bc276769858916ed6c0ff05551c0cdb1e399e79
Maverick <maverick.widjaja@haw-hamburg.de>
2025-06-12 15:03:33 +0200

    fixed mutex copy constructor isssue of ContextData

--- src/FSM/Logic.cpp
+++ src/FSM/Logic.cpp
@@ -31,8 +31,8 @@ Logic::~Logic() {
 
 void Logic::threadFunction() {
     int eventNo = 0;
-    ContextData data = ContextData(local_sender, to_self_sender);
-    auto fsm = Context<Boot>(&data);
+    ContextData* data = new ContextData(local_sender, to_self_sender);
+    auto fsm = Context<Boot>(data);
     while(logicRunning) {
         _pulse event;
         local_receiver->receive_event(&event);
@@ -45,7 +45,7 @@ void Logic::threadFunction() {
         }
         fsm.handleEvent(event);
     }
-    //delete timer_sender;
+    delete data;
 }
 
 //===================================================== public functions =====================================================
