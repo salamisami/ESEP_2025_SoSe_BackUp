3c472f236a6fa027aca2ff71c47bd3c1680f1e02
Maverick <maverick.widjaja@haw-hamburg.de>
2025-06-26 09:20:52 +0200

    optimized dynamic gate opening timing in calibration

--- src/FSM/States/Servicemode/Substates/CalRampSlow/Substates/EndToGateCRS.cpp
+++ src/FSM/States/Servicemode/Substates/CalRampSlow/Substates/EndToGateCRS.cpp
@@ -1,5 +1,6 @@
 #include "EndToGateCRS.h"
 
+
 //================================================= constructors & destructors =================================================
 EndToGateCRS::EndToGateCRS(ContextData* data) : State(data) {
     //substate = new SubState(data);
@@ -16,8 +17,8 @@ void EndToGateCRS::entry(){
 	data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_SLOW_ON);
 	data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_LEFT_START);
 	int time_required = data->timeprofile_slow.timestamp[(int) Timestamp::END] - data->timeprofile_slow.timestamp[(int) Timestamp::LASER_GATE_UNBLOCKED];
-	//TODO set the 200 to relative value
-	data->timer->start_timer(time_required - 200, TIMER_ID::CAL_GATE_RAMP1);
+	time_required *= ENDTOGATE_MULTIPLIER;
+	data->timer->start_timer(time_required , TIMER_ID::CAL_GATE_RAMP1);
 }
 
 void EndToGateCRS::exit(){
