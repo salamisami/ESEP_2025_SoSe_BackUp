d12e7d05ded1789b48acc5a80d8900c935d9501b
Sami Askar <sami.askar@haw-hamburg.de>
2025-06-27 14:17:14 +0200

    com to dispatcher message filtering fix

--- src/FSM/Logic.cpp
+++ src/FSM/Logic.cpp
@@ -42,7 +42,7 @@ void Logic::threadFunction() {
         int status = local_receiver->receive_event(&event);
         if(status == 0) {
             eventNo++;
-            printf("Event Code: %d, Event Value: %d\n", event.code, event.value.sival_int);
+            //printf("Event Code: %d, Event Value: %d\n", event.code, event.value.sival_int);
 
 
             int8_t topic = event.code;
