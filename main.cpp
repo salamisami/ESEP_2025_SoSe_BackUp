7741209687c371d23f0f52602eb685034e849035
Maverick <maverick.widjaja@haw-hamburg.de>
2025-07-02 13:42:52 +0200

    modified main to remove some dependant components

--- main.cpp
+++ main.cpp
@@ -20,32 +20,6 @@
 #define COUT(msg) std::cout << msg << std::endl
 using namespace std;
 
-//volatile int lastCommand = 0;
-//void on_command(const char* payload) {
-//    printf("Befehl empfangen: %s\n", payload);
-//    if (strcmp(payload, "start") == 0) {
-//    	lastCommand = 1;
-////        mqtt_festo_publish("festo/anlage1-2/status/Start", "1");
-////        mqtt_festo_publish("festo/anlage1-2/console", "Anlage gestartet");
-//    } else if (strcmp(payload, "stop") == 0) {
-//    	lastCommand = 2;
-////        mqtt_festo_publish("festo/anlage1-2/status/Start", "0");
-////        mqtt_festo_publish("festo/anlage1-2/console", "Anlage gestoppt");
-//    } else if (strcmp(payload, "notaus") == 0) {
-//    	lastCommand = 3;
-////        mqtt_festo_publish("festo/anlage1-2/status/Reset", "0");
-////        mqtt_festo_publish("festo/anlage1-2/console", "NOT-AUS aktiviert");
-//        // Hier ggf. deine eigentliche Notaus-Logik ergänzen!
-//    } else if (strcmp(payload, "reset") == 0) {
-//    	lastCommand = 4;
-////        mqtt_festo_publish("festo/anlage1-2/status/Reset", "1");
-////        mqtt_festo_publish("festo/anlage1-2/console", "Anlage zurückgesetzt");
-//    } else {
-//        //mqtt_festo_publish("festo/anlage1-2/console", "Unbekannter Befehl!");
-//    }
-//}
-
-
 
 int main() {
     cout << "Starting Program..." << endl; // prints Hello World!!!
@@ -61,7 +35,7 @@ int main() {
 
     Thread_COM::Receiver* fsm_receiver = new Thread_COM::Receiver(FBM_N_FSM);
     Thread_COM::Sender* fsm_sender = new Thread_COM::Sender(FBM_N_DISPATCHER);
-    //Thread_COM::Receiver* recorder_receiver = new Thread_COM::Receiver(FBM_N_RECORDER);
+    //Thread_COM::Receiver* recorder_receiver = new Thread_COM::Receiver(FBM_N_RECORDER); //comment this to test without recorder
     Thread_COM::Sender* recorder_sender = new Thread_COM::Sender(FBM_N_DISPATCHER);
 
     //Thread_COM::Receiver* RemCon_receiver = new Thread_COM::Receiver(FBM_N_REMOTE); //comment this to test without RC
@@ -75,48 +49,12 @@ int main() {
     Thread_COM::Sender* hal_sender = new Thread_COM::Sender(FBM_N_DISPATCHER);
 
 
-    // Timestamp slow: 6707
-    // Timestamp slow: 7987
-    // Timestamp slow: 11583
-    // Timestamp slow: 13239
-    // Timestamp slow: 19122
-    // Timestamp slow: 11847
-    // TimeProfile fast_profile = { {2165, 2560, 3651, 4150, 5908, 4050} };
-    // TimeProfile slow_profile = { {6707, 7987, 11583, 13239, 19122, 11847} };
-
-    // int tick_duration = 10;
-    // Piece* piece = new Piece(slow_profile, fast_profile, tick_duration);
-    // piece->slow();
-    // while(true){
-    //     std::cout << "Area: " << (int) piece->getArea() << ", " << "Position: " << (double) piece->getPosition() << std::endl;
-    //     WAIT(500);
-    // }
-
-
-
-
-
-
-
-    auto logic = new Logic<EStopTest>(fsm_receiver, fsm_sender);
+    auto logic = new Logic<Boot>(fsm_receiver, fsm_sender);
     //Recorder* rec = new Recorder(recorder_receiver, recorder_sender);
     //Remote_Controller* remcon = new Remote_Controller(RemCon_receiver, rc_sender); //comment this to test without RC
     COM* externCommunication = new COM(com_external_receiver, FBM_N_COM_EXT, com_receiver_local, com_sender_local);
     externCommunication->start();
-
-    // Thread_COM::Sender* senderDispatcher = new Thread_COM::Sender(FBM_N_DISPATCHER);
-    // int8_t comCode = (int8_t) Topic::COM;
-    // int value = (int) COM_Enum::BUTTON_ESTOP_PRESSED;
-
     HAL* hal = new HAL(hal_receiver, hal_sender);
-
-    /*while (true){
-    senderDispatcher-> send_event(comCode, value);
-    auto remaining = std::chrono::milliseconds(5000);
-    COUT("sending Estop.");
-    std::this_thread::sleep_for(remaining);
-    }*/
-    // WAIT(3000);
 
     // delete def;
     // delete abc;
