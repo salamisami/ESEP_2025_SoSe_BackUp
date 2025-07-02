06346f9a0de37f476474d45672e10c649204a092
Sami <saskar301@gmail.com>
2025-07-02 17:29:42 +0200

    implemented update id

--- src/FSM/States/Operating/Substates/MotorControl/Substates/Idle.cpp
+++ src/FSM/States/Operating/Substates/MotorControl/Substates/Idle.cpp
@@ -13,6 +13,7 @@ Idle::~Idle() {}
 //===================================================== public functions =====================================================
 void Idle::entry(){
 	data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_STOP);
+  data->
 	PRINT_STATE;
 }
 
