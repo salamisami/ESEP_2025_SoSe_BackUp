9f95e3496daee781753faa7b7c52b4449f3f77d5
Maverick <maverick.widjaja@haw-hamburg.de>
2025-07-05 15:32:39 +0200

    added temporary operating to connect logics

--- src/FSM/States/Operating/Substates/MotorControl/Substates/Slow.cpp
+++ src/FSM/States/Operating/Substates/MotorControl/Substates/Slow.cpp
@@ -28,6 +28,10 @@ State* Slow::motor_fast(){
     else{
         data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_SLOW_OFF);
         updateData(MotorPieceState::FAST);
+        for (auto& pair : *data->pieces_map) {
+          Piece* piece = pair.second;  // pair.second is the value (Piece*)
+          piece->piece_tracker.fast();               // Call fast() on the Piece*
+        }
         return new Fast(data);
     }
 }
@@ -35,6 +39,10 @@ State* Slow::motor_fast(){
 State* Slow::motor_stop_fsm(){
         data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_SLOW_OFF);
         data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_STOP);
+        for (auto& pair : *data->pieces_map) {
+          Piece* piece = pair.second;  // pair.second is the value (Piece*)
+          piece->piece_tracker.stop();               // Call fast() on the Piece*
+        }
         updateData(MotorPieceState::STOPPED);
         return new Stop(data);
 }
@@ -49,11 +57,15 @@ State *Slow::delete_w_motor()
   else
     {
       data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_SLOW_OFF);
-        
       return new Idle(data);
     }
 }
 State* Slow::motor_slow(){
+  updateData(MotorPieceState::SLOW);
+        for (auto& pair : *data->pieces_map) {
+          Piece* piece = pair.second;  // pair.second is the value (Piece*)
+          piece->piece_tracker.slow();               // Call fast() on the Piece*
+        }
     return new Slow(data);
 }
 
@@ -64,7 +76,6 @@ void Slow::updateData(MotorPieceState motorPieceState) {
         // Remove the ID from the list if it exists
         if (data->workpieceList.contains(id)) {
             data->workpieceList.remove(id);
-            data->workpieces = data->workpieceList.isEmpty();
         } else {
             printf("Warning: Trying to delete ID %d that doesn't exist in workpiece list\n", id);
         }
@@ -74,8 +85,8 @@ void Slow::updateData(MotorPieceState motorPieceState) {
             data->workpieceList.add(id, motorPieceState);
         }
         data->workpieceList.updateStateAll(motorPieceState);
-        data->workpieces = data->workpieceList.isEmpty();
     }
+        data->workpieces = !data->workpieceList.isEmpty();
 }
 
 State* Slow::clone() {
