3d4e7ccbce604809a9b7074de31654d3b9ff5e35
Maverick <maverick.widjaja@haw-hamburg.de>
2025-07-06 22:26:19 +0200

    added extended tranfer start events

--- src/FSM/FBM1/Operating_FBM1/PieceController/Substates/Gate_PT1.cpp
+++ src/FSM/FBM1/Operating_FBM1/PieceController/Substates/Gate_PT1.cpp
@@ -54,8 +54,10 @@ State* Gate_PT1::sort_out() {
 	return new SortingOut_PT1(data, localdata_);
 }
 State* Gate_PT1::let_through() {
+	localdata_.sort_out_fbm2 = false;
 	return new GateEnd_PT1(data, localdata_);
 }
 State* Gate_PT1::sort_out_fbm2() {
+	localdata_.sort_out_fbm2 = true;
 	return new GateEnd_PT1(data, localdata_);
 }
