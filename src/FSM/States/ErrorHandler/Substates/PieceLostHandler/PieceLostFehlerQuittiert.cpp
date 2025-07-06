f14b3d11cc1d0e6deea834376ef80f92324a702a
infway594 <way594@haw-hamburg.de>
2025-07-06 17:47:05 +0200

    Fixed: Motorstart in PieceLostHandler

--- src/FSM/States/ErrorHandler/Substates/PieceLostHandler/PieceLostFehlerQuittiert.cpp
+++ src/FSM/States/ErrorHandler/Substates/PieceLostHandler/PieceLostFehlerQuittiert.cpp
@@ -19,7 +19,7 @@ void PieceLostFehlerQuittiert::entry(){
 }
 
 void PieceLostFehlerQuittiert::exit(){
-    data->sender->send_event((int8_t) Topic::MOTOR_SLOW, (int) Error_Enum::ERROR_BOTH_R_FULL);   
+    data->sender->send_event((int8_t) Topic::MOTOR_SLOW, (int) Error_Enum::ERROR_W_LOST);   
     data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::TRAFFIC_RED_OFF);
     data->sender->send_event((int8_t) Topic::ERROR, (int) Error_Enum::PIECE_LOST_RESOLVED);
     //TODO CODE
