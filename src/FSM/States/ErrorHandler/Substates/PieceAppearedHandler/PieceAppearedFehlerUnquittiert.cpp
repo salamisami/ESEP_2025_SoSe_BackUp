f4d026493daab324ea4761d7b4552f4f82c0e496
infway594 <way594@haw-hamburg.de>
2025-07-06 17:43:51 +0200

    Anpassen von ErrorEventValues auf negative Werte und Änderung von Motorstop und Motorstart

--- src/FSM/States/ErrorHandler/Substates/PieceAppearedHandler/PieceAppearedFehlerUnquittiert.cpp
+++ src/FSM/States/ErrorHandler/Substates/PieceAppearedHandler/PieceAppearedFehlerUnquittiert.cpp
@@ -14,7 +14,7 @@ PieceAppearedFehlerUnquittiert::~PieceAppearedFehlerUnquittiert() {}
 void PieceAppearedFehlerUnquittiert::entry(){
 	PRINT_STATE;
     //TODO CODE
-    data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) Error_Enum::ERROR_C_LOST_MQTT); 
+    data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) Error_Enum::ERROR_W_APPEARED); 
     data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::TRAFFIC_RED_ON_FAST);
 }
 
