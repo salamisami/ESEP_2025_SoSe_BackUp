444c50965e60ec52467cb81994f53d5c1c3a22ea
Sami <saskar301@gmail.com>
2025-07-09 19:40:20 +0200

    check for both flags

--- src/FSM/SharedFSMs/Top_Level_Fsm/ErrorCom.cpp
+++ src/FSM/SharedFSMs/Top_Level_Fsm/ErrorCom.cpp
@@ -12,52 +12,30 @@ void ErrorCom::entry(){}
 void ErrorCom::exit() { }
 
 State* ErrorCom::mqtt_error_resolved() {
-	data->mqtt_resolved = true;
-	if(data->mqtt_resolved && data->com_resolved){
-		if(data->is_estop){
-			State* estop = data->estop_history->top();
-			data->estop_history->pop();
-			return estop;
-		}
-		else{
-			State* modehandler = data->modehandler_history->top();
-			data->modehandler_history->pop();
-			//TODO check for EstopPressed and send Event
-			return modehandler;
-		}
+  data->mqtt_error_resolved = true;
+  if (!com_error_resolved || !mqtt_error_resolved){
+    return nullptr;
+	if(data->is_estop){
+		State* estop = data->estop_history->top();
+		data->estop_history->pop();
+		return estop;
 	}
-	return nullptr;
-	
-	
+	State* modehandler = data->modehandler_history->top();
+	return modehandler;
 }
 
 State* ErrorCom::com_error_resolved() {
-	data->com_resolved = true;
-	if(data->mqtt_resolved && data->com_resolved){
-		if(data->is_estop){
-			State* estop = data->estop_history->top();
-			data->estop_history->pop();
-			return estop;
-		}
-
-		else{
-			State* modehandler = data->modehandler_history->top();
-			data->modehandler_history->pop();
-			//TODO check for EstopPressed and send Event
-			return modehandler;
-		}
+  data->com_error_resolved = true;
+  if (!com_error_resolved || !mqtt_error_resolved){
+    return nullptr;
+  }
+	if(data->is_estop){
+		State* estop = data->estop_history->top();
+		data->estop_history->pop();
+		return estop;
 	}
-	return nullptr;
-}
-
-State* ErrorCom::error_c_lost_mqtt(){
-	data->mqtt_resolved = false;
-	return nullptr;
-}
-
-State* ErrorCom::error_c_lost_com(){
-	data->com_resolved = false;
-	return nullptr;
+	State* modehandler = data->modehandler_history->top();
+	return modehandler;
 }
 
 State* ErrorCom::clone() {
