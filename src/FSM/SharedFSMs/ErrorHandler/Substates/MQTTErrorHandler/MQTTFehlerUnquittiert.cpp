dac59cd210e17b373bb5f3c1cb616a24912ca783
Maverick <maverick.widjaja@haw-hamburg.de>
2025-07-09 09:44:18 +0200

    added led feature to reset

--- src/FSM/SharedFSMs/ErrorHandler/Substates/MQTTErrorHandler/MQTTFehlerUnquittiert.cpp
+++ src/FSM/SharedFSMs/ErrorHandler/Substates/MQTTErrorHandler/MQTTFehlerUnquittiert.cpp
@@ -16,12 +16,14 @@ void MQTTFehlerUnquittiert::entry(){
 	PRINT_STATE;
     //TODO CODE
     data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::TRAFFIC_RED_ON_FAST);
+    data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::TRAFFIC_GREEN_ON_SLOW);
     data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) Error_Enum::ERROR_C_LOST_MQTT);
+    data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::LED_RESET_ON);
 }
 
 void MQTTFehlerUnquittiert::exit(){
-    //TODO CODE
 	PRINT_STATE;
+    data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::LED_RESET_OFF);
 }
 
 State* MQTTFehlerUnquittiert::button_reset_released()
