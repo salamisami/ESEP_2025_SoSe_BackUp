d55f9bd1e92e39b298784ef1769eec76b3f3e7d6
Sami <saskar301@gmail.com>
2025-07-12 20:44:54 +0200

    [MOTOR] set desired state of workpiece before updating

--- src/FSM/FBM1/Operating_FBM1/MotorControl/Substates/Stop.cpp
+++ src/FSM/FBM1/Operating_FBM1/MotorControl/Substates/Stop.cpp
@@ -1,73 +1,59 @@
 #include "Stop.h"
 
-//================================================= constructors & destructors =================================================
-Stop::Stop(ContextData *data) : State(data)
-{
-    // substate = new SubState(data);
+//================================================= constructors & destructors
+//=================================================
+Stop::Stop(ContextData *data) : State(data) {
+  // substate = new SubState(data);
 }
 
 Stop::~Stop() {}
 
-//===================================================== private functions =====================================================
+//===================================================== private functions
+//=====================================================
 
-//===================================================== public functions =====================================================
-void Stop::entry()
-{
-    PRINT_STATE;
-    MotorControl::updateData(data, MotorPieceState::STOPPED);
-    data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::MOTOR_SLOW_OFF, (int) EventPriority::SECOND_PRIO);
-    data->sender->send_event((int8_t)Topic::ACTUATOR, (int)ActuatorEnum::MOTOR_STOP, (int)EventPriority::SECOND_PRIO);
+//===================================================== public functions
+//=====================================================
+void Stop::entry() {
+  PRINT_STATE;
+  MotorControl::updateData(data, MotorPieceState::STOPPED);
+  data->sender->send_event((int8_t)Topic::ACTUATOR,
+                           (int)ActuatorEnum::MOTOR_SLOW_OFF,
+                           (int)EventPriority::SECOND_PRIO);
+  data->sender->send_event((int8_t)Topic::ACTUATOR,
+                           (int)ActuatorEnum::MOTOR_STOP,
+                           (int)EventPriority::SECOND_PRIO);
 }
 
-void Stop::exit()
-{
-    PRINT_STATE;
+void Stop::exit() { PRINT_STATE; }
+
+State *Stop::delete_w_motor() {
+  MotorControl::updateData(data, MotorPieceState::DELETE_W_MOTOR);
+  if (data->workpieces) {
+    return nullptr;
+  } else {
+
+    return new Idle(data);
+  }
 }
 
-State *Stop::delete_w_motor()
-{
-    MotorControl::updateData(data, MotorPieceState::DELETE_W_MOTOR);
-    if (data->workpieces)
-    {
-        return nullptr;
-    }
-    else
-    {
-
-        return new Idle(data);
-    }
-}
-
-State *Stop::motor_slow()
-{
-    if (data->motor_stopped)
-    {
-        return new Stop(data);
-    }
-    else
-    {
-        return new Slow(data);
-    }
-}
-
-State *Stop::motor_fast()
-{
-    if (data->motor_stopped)
-    {
-        return new Stop(data);
-    }
-    else
-    {
-        return new Fast(data);
-    }
-}
-
-State *Stop::motor_stop_fsm()
-{
+State *Stop::motor_slow() {
+  data->workpieceList.updateState(data->event_payload, MotorPieceState::SLOW);
+  if (data->motor_stopped) {
     return new Stop(data);
+  } else {
+    return new Slow(data);
+  }
 }
 
-State *Stop::clone()
-{
+State *Stop::motor_fast() {
+  data->workpieceList.updateState(data->event_payload, MotorPieceState::FAST);
+  if (data->motor_stopped) {
     return new Stop(data);
+  } else {
+    return new Fast(data);
+  }
 }
+
+State *Stop::motor_stop_fsm() { return new Stop(data); }
+
+State *Stop::clone() { return new Stop(data); }
