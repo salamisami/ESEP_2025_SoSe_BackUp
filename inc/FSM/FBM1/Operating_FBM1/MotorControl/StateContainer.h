3a5ac8d9337f709bcdef7fc558dc93580eda3aa7
Sami <saskar301@gmail.com>
2025-07-12 22:41:14 +0200

    added throw for testing

--- inc/FSM/FBM1/Operating_FBM1/MotorControl/StateContainer.h
+++ inc/FSM/FBM1/Operating_FBM1/MotorControl/StateContainer.h
@@ -1,77 +1,74 @@
 #ifndef STATECONTAINER_H
 #define STATECONTAINER_H
 
+#include <stdexcept>
 #include <unordered_map>
 #include <vector>
-#include <stdexcept>
 
-enum class MotorPieceState {
-    FAST,
-    SLOW,
-    STOPPED,
-    DELETE_W_MOTOR
-};
+enum class MotorPieceState { FAST, SLOW, STOPPED, DELETE_W_MOTOR };
 
 class StateContainer {
-    std::unordered_map<int, MotorPieceState> container;
-    
+  std::unordered_map<int, MotorPieceState> container;
+
 public:
-    void add(int id, MotorPieceState motorPieceState) {
-        if (!container.emplace(id, motorPieceState).second) {
-//            throw std::runtime_error("id already exists");
-        }
+  void add(int id, MotorPieceState motorPieceState) {
+    if (!container.emplace(id, motorPieceState).second) {
+#ifdef TEST_MOTOR_THROW
+      throw std::runtime_error("id already exists");
+#endif
     }
-    
-    void remove(int id) {
-        if (container.erase(id) == 0) {
-//            throw std::runtime_error("ID not found");
-        }
+  }
+
+  void remove(int id) {
+    if (container.erase(id) == 0) {
+#ifdef TEST_MOTOR_THROW
+      throw std::runtime_error("ID not found");
+#endif
     }
-    
-    void updateState(int id, MotorPieceState newState) {
-        auto it = container.find(id);
-        if (it != container.end()) {
-            it->second = newState;
-        } else {
- //           throw std::runtime_error("ID not found");
-        }
+  }
+
+  void updateState(int id, MotorPieceState newState) {
+    auto it = container.find(id);
+    if (it != container.end()) {
+      it->second = newState;
+    } else {
+#ifdef TEST_MOTOR_THROW
+      throw std::runtime_error("ID not found");
+#endif
     }
-    
-    void updateStateAll(MotorPieceState newState) {
-        for (auto& pair : container) {
-            pair.second = newState;
-        }
+  }
+
+  void updateStateAll(MotorPieceState newState) {
+    for (auto &pair : container) {
+      pair.second = newState;
     }
-    
-    MotorPieceState getState(int id) const {
-        auto it = container.find(id);
-        if (it != container.end()) {
-            return it->second;
-        }
-  //      throw std::runtime_error("ID not found");
+  }
+
+  MotorPieceState getState(int id) const {
+    auto it = container.find(id);
+    if (it != container.end()) {
+      return it->second;
+    }
+#ifdef TEST_MOTOR_THROW
+    throw std::runtime_error("ID not found");
+#endif
     return (MotorPieceState)-1;
+  }
+
+  bool contains(int id) const { return container.find(id) != container.end(); }
+
+  std::vector<int> getAllIds() const {
+    std::vector<int> ids;
+    ids.reserve(container.size());
+    for (const auto &pair : container) {
+      ids.push_back(pair.first);
     }
-    
-    bool contains(int id) const {
-        return container.find(id) != container.end();
-    }
-    
-    std::vector<int> getAllIds() const {
-        std::vector<int> ids;
-        ids.reserve(container.size());
-        for (const auto& pair : container) {
-            ids.push_back(pair.first);
-        }
-        return ids;
-    }
-    
-    size_t size() const {
-        return container.size();
-    }
-    
-    bool isEmpty() const {
-        return container.empty();
-    }
+    return ids;
+  }
+
+  size_t size() const { return container.size(); }
+
+  bool isEmpty() const { return container.empty(); }
 };
 
 #endif // STATECONTAINER_H
