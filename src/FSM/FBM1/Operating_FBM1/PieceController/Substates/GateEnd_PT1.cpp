fc8cb5207054e5b2beebdbaaf1096bea53872d60
Maverick <maverick.widjaja@haw-hamburg.de>
2025-07-12 17:58:56 +0200

    17:52 12.07.2025

--- src/FSM/FBM1/Operating_FBM1/PieceController/Substates/GateEnd_PT1.cpp
+++ src/FSM/FBM1/Operating_FBM1/PieceController/Substates/GateEnd_PT1.cpp
@@ -26,21 +26,28 @@ State* GateEnd_PT1::clone() {
 }
 
 State* GateEnd_PT1::timer(TIMER_ID id) {
-	if ( id != TIMER_ID::GATEEND_PT1){
+	if(id != TIMER_ID::GATEEND_PT1) {
 		return nullptr;
 	}
 	auto piece = localdata_.piece;
 	auto distance = piece->piece_tracker->get_distance();
 	Area current_area = distance.first;
-	
-	if(current_area == Area::OUT_OF_RANGE){// && distance.second >= PIECE_TRANSITION_TOLERANCE) {
-		DEBUG("PieceMissing! Cause: piece is too long to reach laser back.");
-	  MACRO_PIECE_MISSING_PT1
-  }
 
-	if(data->piece_near_adc && distance.second >= DISTANCE_BETWEEN_PIECES ) {
-		data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) localdata_.piece->id);
-		return new 	PendingTransferRequestNotAtEnd(data, localdata_);
+	switch(current_area) {
+		case Area::GATE_END: {
+				if(data->piece_near_adc) {
+					data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) localdata_.piece->id);
+					return new PendingTransferRequestNotAtEnd(data, localdata_);
+				}
+				break;
+			}
+		case Area::OUT_OF_RANGE: {
+				DEBUG("PieceMissing! Cause: piece is too long to reach laser back.");
+				MACRO_PIECE_MISSING_PT1
+					break;
+			}
+		default:
+			break;
 	}
 	return new GateEnd_PT1(data, localdata_);
 }
@@ -55,10 +62,10 @@ State* GateEnd_PT1::laser_back_blocked() {
 		data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) localdata_.piece->id);
 		return new PendingTransferRequest_PT1(data, localdata_);
 	}
-	 if(current_area == Area::OUT_OF_RANGE) {
-	 	data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) localdata_.piece->id);
-	 	return new PendingTransferRequest_PT1(data, localdata_);
-	 }
+	if(current_area == Area::OUT_OF_RANGE) {
+		data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) localdata_.piece->id);
+		return new PendingTransferRequest_PT1(data, localdata_);
+	}
 
 	return nullptr;
 }
