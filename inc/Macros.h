67ff5a6b7be9a0bc2f59e8da67a1d4600d24b4e7
Sami <saskar301@gmail.com>
2025-07-13 16:35:16 +0200

    [MotorControl] Adjustment for updated design

--- inc/Macros.h
+++ inc/Macros.h
@@ -2,26 +2,25 @@
 #define MACROS_H
 #pragma once
 
-#include <stdexcept>
-#include <string>
 #include <chrono>
-#include <thread>
 #include <iostream>
-
+#include <stdexcept>
+#include <string>
+#include <thread>
 
 #include <stdexcept>
 #include <string>
 
-#define THROW(msg) throw std::runtime_error( \
-    "Exception is occured at line: " + std::to_string(__LINE__) + " in " + __FILE__ + ": " + std::string(msg) \
-)
+#define THROW(msg)                                                             \
+  throw std::runtime_error(                                                    \
+      "Exception is occured at line: " + std::to_string(__LINE__) + " in " +   \
+      __FILE__ + ": " + std::string(msg))
 
 #ifdef MOCK
 #define WAIT(x) std::this_thread::sleep_for(std::chrono::milliseconds(x));
 #else
 #define WAIT(x) usleep(x * 1000)
 #endif
-
 
 #define FILE_EXISTS(fname) (std::ifstream(fname).good())
 
@@ -53,52 +52,65 @@
 #define FBM_2
 // Configuration for FBM Module 1
 #ifdef FBM_1
-	#define FBM					1
-	#define FBM_N_HAL       	"Hal_1"
-	#define FBM_N_FSM       	"Fsm_1"
-	#define FBM_N_COM       	"Com_1"
-	#define FBM_N_COM_EXT		"Com_2"
-	#define FBM_N_COM_RECEIVER	"Com_receiver_1"
-	#define FBM_N_RECORDER  	"Rec_1"
-	#define FBM_N_REMOTE    	"Rem_1"
-	#define FBM_N_DISPATCHER 	"Dis_1"
-	#define RECEIVE_TOPIC		"festo/anlage1/status/"
-	#define COMMAND_TOPIC		"festo/anlage1/command"
-	#define MQTT_CLIENT			"Festo_FBM1"
+#define FBM 1
+#define FBM_N_HAL "Hal_1"
+#define FBM_N_FSM "Fsm_1"
+#define FBM_N_COM "Com_1"
+#define FBM_N_COM_EXT "Com_2"
+#define FBM_N_COM_RECEIVER "Com_receiver_1"
+#define FBM_N_RECORDER "Rec_1"
+#define FBM_N_REMOTE "Rem_1"
+#define FBM_N_DISPATCHER "Dis_1"
+#define RECEIVE_TOPIC "festo/anlage1/status/"
+#define COMMAND_TOPIC "festo/anlage1/command"
+#define MQTT_CLIENT "Festo_FBM1"
 #endif
 
 #ifdef FBM_2
-	// Default to FBM Module 2
-	#define FBM					2
-	#define FBM_N_HAL       	"Hal_2"
-	#define FBM_N_FSM       	"Fsm_2"
-	#define FBM_N_COM       	"Com_2"
-	#define FBM_N_COM_EXT		"Com_1"
-	#define FBM_N_COM_RECEIVER 	"Com_receiver_2"
-	#define FBM_N_RECORDER  	"Rec_2"
-	#define FBM_N_REMOTE   		"Rem_2"
-	#define FBM_N_DISPATCHER 	"Dis_2"
-	#define RECEIVE_TOPIC		"festo/anlage2/status/"
-	#define COMMAND_TOPIC		"festo/anlage2/command"
-	#define MQTT_CLIENT			"Festo_FBM2"
+// Default to FBM Module 2
+#define FBM 2
+#define FBM_N_HAL "Hal_2"
+#define FBM_N_FSM "Fsm_2"
+#define FBM_N_COM "Com_2"
+#define FBM_N_COM_EXT "Com_1"
+#define FBM_N_COM_RECEIVER "Com_receiver_2"
+#define FBM_N_RECORDER "Rec_2"
+#define FBM_N_REMOTE "Rem_2"
+#define FBM_N_DISPATCHER "Dis_2"
+#define RECEIVE_TOPIC "festo/anlage2/status/"
+#define COMMAND_TOPIC "festo/anlage2/command"
+#define MQTT_CLIENT "Festo_FBM2"
 #endif
-#define MACRO_PIECE_MISSING_PT1 data->sender->send_event((int8_t) Topic::ERROR, (int) Error_Enum::ERROR_W_LOST);\
-	data->sender->send_event((int8_t) Topic::DELETE_W_MOTOR, (int) localdata_.piece->id);\
-	PieceEnum validated_piece = localdata_.validated_type;\
-	switch(validated_piece) {\
-		case PieceEnum::FLAT:\
-			data->sender->send_event((int8_t) Topic::INTERNAL, (int) Internal_Enum::RESET_TO_FLAT);\
-			break;\
-		case PieceEnum::TALL:\
-			data->sender->send_event((int8_t) Topic::INTERNAL, (int) Internal_Enum::RESET_TO_TALL);\
-			break;\
-		case PieceEnum::TALL_WITH_METAL:\
-			data->sender->send_event((int8_t) Topic::INTERNAL, (int) Internal_Enum::RESET_TO_TALL_W_METAL);\
-			break;\
-		default:\
-			break;\
-	}\
-	Piece* piece_to_delete = localdata_.piece;\
-	data->pieces_map->erase(localdata_.piece->id);\
-	delete piece_to_delete;\
-	return State::EXIT_STATE;
+#define MACRO_PIECE_MISSING_PT1                                                \
+  data->sender->send_event((int8_t)Topic::ERROR,                               \
+                           (int)Error_Enum::ERROR_W_LOST);                     \
+  data->sender->send_event((int8_t)Topic::DELETE_W_MOTOR,                      \
+                           (int)localdata_.piece->id);                         \
+  PieceEnum validated_piece = localdata_.validated_type;                       \
+  switch (validated_piece) {                                                   \
+  case PieceEnum::FLAT:                                                        \
+    data->sender->send_event((int8_t)Topic::INTERNAL,                          \
+                             (int)Internal_Enum::RESET_TO_FLAT);               \
+    break;                                                                     \
+  case PieceEnum::TALL:                                                        \
+    data->sender->send_event((int8_t)Topic::INTERNAL,                          \
+                             (int)Internal_Enum::RESET_TO_TALL);               \
+    break;                                                                     \
+  case PieceEnum::TALL_WITH_METAL:                                             \
+    data->sender->send_event((int8_t)Topic::INTERNAL,                          \
+                             (int)Internal_Enum::RESET_TO_TALL_W_METAL);       \
+    break;                                                                     \
+  default:                                                                     \
+    break;                                                                     \
+  }                                                                            \
+  Piece *piece_to_delete = localdata_.piece;                                   \
+  data->pieces_map->erase(localdata_.piece->id);                               \
+  delete piece_to_delete;                                                      \
+  return State::EXIT_STATE;
+
+// In your MotorStateMacros.h or similar header
+#define AREA_AS_INT_TO_STATE(data, state_int)                                  \
+  ((state_int) == 0)   ? static_cast<State *>(new Fast(data))                  \
+  : ((state_int) == 1) ? static_cast<State *>(new Slow(data))                  \
+  : ((state_int) == 2) ? static_cast<State *>(new Stop(data))                  \
+                       : nullptr
