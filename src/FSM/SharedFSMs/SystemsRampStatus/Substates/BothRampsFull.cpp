fd00ad944db48c0f28050970cb0ffa27aa7d776b
infway594 <way594@haw-hamburg.de>
2025-07-14 16:43:03 +0200

    added sending event in BothRampsFull

--- src/FSM/SharedFSMs/SystemsRampStatus/Substates/BothRampsFull.cpp
+++ src/FSM/SharedFSMs/SystemsRampStatus/Substates/BothRampsFull.cpp
@@ -35,13 +35,14 @@ State* BothRampsFull::ramp_not_full()
 
 State* BothRampsFull::sorting_out()
 {
+    data->sender->send_event((int8_t)Topic::ERROR, (int) Error_Enum::ERROR_BOTH_R_FULL);
     return new BothRampsFull(data);
 }
 
-/* State* BothRampsFull::error_both_r_full()
+State* BothRampsFull::error_both_r_full()
 {
     return new BothRampsFull(data);
-} */
+}
 
 State* BothRampsFull::clone() {
     return new BothRampsFull(data);
