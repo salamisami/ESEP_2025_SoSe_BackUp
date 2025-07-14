86d938e90c5cd4174decc18d2908c0c04bcbe194
Robin Pankonin <@infwou228>
2025-07-14 13:11:40 +0200

    back to old adc

--- src/HAL/Components/ADC_Utils/ADC_Utilities.cpp
+++ src/HAL/Components/ADC_Utils/ADC_Utilities.cpp
@@ -68,7 +68,7 @@ void ADC_Utilities::calibrateComponents(ADC& adc, TSCADC& tscadc, float bandVolt
             if (bauteilErkannt) {
                 werte.push_back(sensorVoltage);
                 int messpunkte = werte.size();
-                if ((sensorVoltage > bandVoltage - TRIGGER_SCHRITT) && messpunkte >= W_SIZE) {
+                if (sensorVoltage > bandVoltage - TRIGGER_SCHRITT){// && messpunkte >= W_SIZE) {
                     break;
                 }
             }
@@ -352,7 +352,7 @@ ADC_Enum ADC_Utilities::executeMeasurement(ADC& adc, TSCADC& tscadc, float bandV
 
         werte.push_back(sensorVoltage);
         int messpunkte = werte.size();
-        if((sensorVoltage > bandVoltage) - TRIGGER_SCHRITT && messpunkte >= W_SIZE) {
+        if(sensorVoltage > bandVoltage - TRIGGER_SCHRITT){//} && messpunkte >= W_SIZE) {
             break;
         }
         if(werte.size() >= MAX_WERT) {
