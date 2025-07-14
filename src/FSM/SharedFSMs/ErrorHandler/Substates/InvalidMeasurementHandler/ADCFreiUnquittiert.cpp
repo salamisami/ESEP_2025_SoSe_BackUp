0667d603cd6ddd47438e0b0e2c986d043899ae63
infwba291 <Thomas.Haag@haw-hamburg.de>
2025-07-15 01:39:31 +0200

    Invalid Meassure Handler nutzt jetzt das Error_Enum als ID beim MotorController

--- src/FSM/SharedFSMs/ErrorHandler/Substates/InvalidMeasurementHandler/ADCFreiUnquittiert.cpp
+++ src/FSM/SharedFSMs/ErrorHandler/Substates/InvalidMeasurementHandler/ADCFreiUnquittiert.cpp
@@ -19,7 +19,7 @@ void ADCFreiUnquittiert::entry(){
 
 void ADCFreiUnquittiert::exit(){
     data->sender->send_event((int8_t)Topic::ACTUATOR, (int)ActuatorEnum::TRAFFIC_RED_OFF);
-    data->sender->send_event((int8_t)Topic::DELETE_W_MOTOR, data->event_payload);
+    data->sender->send_event((int8_t)Topic::DELETE_W_MOTOR, Error_Enum::ERROR_INVALID_MESURE);
     data->sender->send_event((int8_t)Topic::ERROR, (int) Error_Enum::ERROR_INVALID_MEASURE_RESOLVED);
     data->error_warning_counter->error_or_warning_resolved();
     
