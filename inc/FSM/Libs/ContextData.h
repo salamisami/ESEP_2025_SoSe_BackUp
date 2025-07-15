5fb17a2ee0960a1980d7ba10812024801bc60559
Maverick <maverick.widjaja@haw-hamburg.de>
2025-07-15 14:23:46 +0200

    optimized sorting ou ttime

--- inc/FSM/Libs/ContextData.h
+++ inc/FSM/Libs/ContextData.h
@@ -45,7 +45,7 @@ public:
   std::stack<State *> *estop_history;
 
   std::unordered_map<int, Piece *> *pieces_map;
-  int available_id = 1;
+  int available_id = 50;
   Piece *piece_FBM2_soll = nullptr;
   // Piece* piece_FBM2_ist = nullptr;
 
