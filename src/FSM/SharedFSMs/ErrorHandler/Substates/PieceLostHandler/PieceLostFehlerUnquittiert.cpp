41c0d3088e48c1ba76ad1e824d57f6719955bb6e
infwba291 <Thomas.Haag@haw-hamburg.de>
2025-07-15 02:44:52 +0200

    PieceLost Handler added Reset Led

--- src/FSM/SharedFSMs/ErrorHandler/Substates/PieceLostHandler/PieceLostFehlerUnquittiert.cpp
+++ src/FSM/SharedFSMs/ErrorHandler/Substates/PieceLostHandler/PieceLostFehlerUnquittiert.cpp
@@ -17,6 +17,7 @@ void PieceLostFehlerUnquittiert::entry(){
     
     data->sender->send_event((int8_t) Topic::MOTOR_STOP_FSM, (int) Error_Enum::ERROR_W_LOST); 
     data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::TRAFFIC_RED_ON_FAST);
+    data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::LED_RESET_ON);
     data->error_warning_counter->error_or_warning_occured();
 }
 
