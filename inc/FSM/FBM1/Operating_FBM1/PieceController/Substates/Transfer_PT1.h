16b64900943a81411be3e0517fdaf3cbac3cd383
Sami <saskar301@gmail.com>
2025-07-15 22:18:44 +0200

    added timeout transfer_pt1

--- inc/FSM/FBM1/Operating_FBM1/PieceController/Substates/Transfer_PT1.h
+++ inc/FSM/FBM1/Operating_FBM1/PieceController/Substates/Transfer_PT1.h
@@ -2,38 +2,37 @@
 #define TRANSFER_PT1_H
 #pragma once
 
-#include "State.h"
 #include "LocalDataPT1.h"
+#include "Piece.h"
+#include "State.h"
 
 class Transfer_PT1 : public State {
-public: //============================================ constructors & destructors ============================================
-    Transfer_PT1(ContextData* data, LocalDataPT1 localdata) ;
-    virtual ~Transfer_PT1();
-	
+public: //============================================ constructors &
+        // destructors ============================================
+  Transfer_PT1(ContextData *data, LocalDataPT1 localdata);
+  virtual ~Transfer_PT1();
 
-public: //================================================ public functions ================================================
-    void entry() override;
-    void exit() override;
-	State* clone() override;
+public: //================================================ public functions
+        //================================================
+  void entry() override;
+  void exit() override;
+  State *clone() override;
 
-	State* transfer_done() override;
-	State* transfer_failed() override;
-    
+  State *transfer_done() override;
+  State *transfer_failed() override;
+  State *timer(TIMER_ID id) override;
 
+private: //================================================ private variables
+         //================================================
+  // classes, STL containers, and structs
+  // pointers
+  // primitive types
+  LocalDataPT1 localdata_;
+  // bool and char
 
-private: //================================================ private variables ================================================
-	//classes, STL containers, and structs
-	//pointers
-	//primitive types
-	LocalDataPT1 localdata_;
-	//bool and char
-   
-	
-
-private: //================================================ private functions ================================================
-	//void privateFunction();
-	
-	
+private
+    : //================================================ private functions
+      //================================================ void privateFunction();
 };
 
 #endif
