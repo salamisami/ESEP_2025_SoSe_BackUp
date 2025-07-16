b8229c2ef4bd69822e88bcafc69f48ea4f52ad01
Sahel <sahel.sediqi@haw-hamburg.de>
2025-07-16 09:34:26 +0200

    idle reset led fix

--- src/FSM/SharedFSMs/ModeHandler/IdleIM.cpp
+++ src/FSM/SharedFSMs/ModeHandler/IdleIM.cpp
@@ -4,7 +4,7 @@ void IdleIM::entry() {
     data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::LED_START_ON);
     PRINT_STATE;
     data->sender->send_event((int8_t) Topic::ACTUATOR, (int) ActuatorEnum::TRAFFIC_GREEN_ON_SLOW);
-    data->sender->send_event((int8_t)Topic::ACTUATOR, (int)ActuatorEnum::TRAFFIC_GREEN_OFF);
+    data->sender->send_event((int8_t)Topic::ACTUATOR, (int)ActuatorEnum::LED_RESET_OFF);
 }
 
 void IdleIM::exit() {
